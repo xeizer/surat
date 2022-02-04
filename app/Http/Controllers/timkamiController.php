5c2ad52d563843ac2f537adf8acd4a1646a1e48c
Oliv-e <oliverdillon10@gmail.com>
2022-02-05 01:12:08 +0700

    CRUD Tim Pengembang

--- app/Http/Controllers/timkamiController.php
+++ app/Http/Controllers/timkamiController.php
@@ -9,14 +9,15 @@ use Illuminate\Support\Facades\DB;
 class timkamiController extends Controller
 {
     public function index() {
-        $timkami = timkami::get();
-        return view('timkami', compact('timkami'));
+        $timkami = timkami::orderBy('nama')->get();
+        return view('tim.timkami', compact('timkami'));
     }
     public function upload(Request $request) {
         $this->validate($request, [
             'nama' => 'required',
             'jabatan' => 'required',
         ]);
+
         $upload = timkami::create([
             'nama' => $request->nama,
             'jabatan' => $request->jabatan,
@@ -29,12 +30,28 @@ class timkamiController extends Controller
         return redirect('/timkami');
     }
     public function edit($id) {
-
+        $timkami = timkami::find($id);
+        return view('tim/timedit', compact('timkami'));
     }
-    public function hapus($id) {
-        $tim = timkami::findorfail($id);
-        $tim->delete();
-
+    public function update(Request $request, $id) {
+        $this->validate($request, [
+            'nama' => 'required',
+            'jabatan' => 'required',
+        ]);
+        $upload = timkami::find($id)->update([
+            'nama' => $request->nama,
+            'jabatan' => $request->jabatan,
+            'github' => $request->github,
+            'facebook' => $request->facebook,
+            'instagram' => $request->instagram,
+            'youtube' => $request->youtube,
+            'twitter' => $request->twitter,
+        ]);
+        return redirect('/timkami');
+    }
+    public function destroy($id) {
+        $timkami = timkami::find($id);
+        $timkami->delete();
         return redirect('/timkami');
     }
 }
